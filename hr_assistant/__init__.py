7a034b40e45d4ef53c3eee97b4fc3c951967e223
gege-xia <GEGEXIA94@GMAIL.COM>
2026-05-24 10:57:42 +0200

    hr-assistant avanzamento 01 - lettura curriculum

--- hr_assistant/__init__.py
+++ hr_assistant/__init__.py
@@ -1,7 +1,30 @@
+import os
+import uuid
 import chainlit as cl
 
+def load_resumes(documents_dir="resumes"):
+    documents = []
+    metadatas = []
+    ids = []
+    
+    for filename in os.listdir(documents_dir):
+        if filename.endswith(".txt"):
+            path = os.path.join(documents_dir, filename)
+            with open(path, "r", encoding="utf-8") as file:
+                chunks = file.read().replace("\n", ". ").split("### ")
+                
+                for chunk in chunks:
+                    if chunk.strip():
+                        documents.append(chunk.strip())
+                        metadatas.append({"source": filename})
+                        ids.append(str(uuid.uuid4()))
+                        
+    return documents, metadatas, ids
+documents, metadatas, ids = load_resumes()
+sources = set(item["source"] for item in metadatas) 
+    
 @cl.on_message
 async def main(message: cl.Message):
-    await cl.Message(
-        content=f"Ciao, mi hai scritto: {message.content}"
-    ).send()
+        await cl.Message(
+        content=f"Ho caricato {len(documents)} blocchi CV da {len(sources)} file."
+        ).send()       
